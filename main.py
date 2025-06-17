ecfa1695e743d296aa97480fec64e3c7ff8149f1
MinerCanseco <miner_canseco@hotmail.com>
2025-06-17 09:12:36 -0600

    primer commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import ttkbootstrap as ttk
+from cayal.parametros_contpaqi import ParametrosContpaqi
+
+
+if __name__ == '__main__':
+    parametros = ParametrosContpaqi()
+    ventana = ttk.Window()
+
